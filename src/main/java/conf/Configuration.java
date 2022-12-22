867e8901bb71454bf72a66c97c56bc9ed7ebdc0e
Denys Ordynskyi <denys.ordynskyi@sigma.software>
2022-12-22 14:22:19 +0200

    Added chromedriver execution

--- src/main/java/conf/Configuration.java
+++ src/main/java/conf/Configuration.java
@@ -10,6 +10,7 @@ public class Configuration {
 
     public static final String EMAIL = get("TA_USER_EMAIL");
     public static final String PASSWORD = get("TA_USER_PASSWORD");
+    public static final String DRIVER_TYPE = get("TA_DRIVER_TYPE");
 
     private static String get(String parameterName) {
         String property = System.getProperty(parameterName);
@@ -17,5 +18,26 @@ public class Configuration {
             property = dotenv.get(parameterName);
         }
         return property;
+    }
+
+    public static String getOSType() {
+        String os = System.getProperty("os.name").toLowerCase();
+        if (os.contains("win")) {
+            return "WINDOWS";
+        } else if (os.contains("mac")) {
+            return "MACOS";
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            return "LINUX";
+        }
+        return null;
+    }
+
+    public static String getWebdriversPath() {
+        if (getOSType() == null) return null;
+        String path = "webdrivers/" + getOSType() + "_" + DRIVER_TYPE;
+        if (getOSType().equals("WINDOWS")) {
+            path += ".exe";
+        }
+        return path;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,46 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static conf.Configuration.*;
 
 public class Main {
+
+    private static WebDriver driver;
     public static void main(String[] args) {
+        if (getOSType() == null || getWebdriversPath() == null) return;
+
+        System.setProperty(
+                "webdriver." +
+                        (DRIVER_TYPE.equals("FIREFOX") ?
+                                "gecko" :
+                                DRIVER_TYPE.toLowerCase())
+                        + ".driver",
+                getWebdriversPath());
+
+        Map<String, Class> driverClasses = Stream.of(new Object[][]{
+                {"CHROME", ChromeDriver.class},
+                {"FIREFOX", FirefoxDriver.class},
+        }).collect(Collectors.toMap(data -> (String) data[0], data -> (Class) data[1]));
+
+        try {
+            driver = (WebDriver) driverClasses.get(DRIVER_TYPE).getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
         System.out.println("EMAIL " + EMAIL);
         System.out.println("PASSWORD " + PASSWORD);
+
+        driver.quit();
     }
 }
